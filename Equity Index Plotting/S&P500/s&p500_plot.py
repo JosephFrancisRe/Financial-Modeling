f6f09285644af573a96862266e355bd868cb5187
Joseph Francis Re <josephfrancisre@gmail.com>
2023-03-05 20:32:47 -0500

    Corrected spelling error

--- Equity Index Plotting/S&P500/s&p500_plot.py
+++ Equity Index Plotting/S&P500/s&p500_plot.py
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 data = pd.read_csv('sp500_1950_mon.csv')
 
 plt.title('S&P500 Adjusted Close Price Since 1950')
-plt.ylabel('Adjusted Clase Price')
+plt.ylabel('Adjusted Close Price')
 plt.xlabel('Months')
 plt.plot(data['Adj Close'])
 plt.savefig('sp500_close_price_1950_thru_2018.png', bbox_inches='tight')
