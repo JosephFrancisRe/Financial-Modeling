284a53d57bcbb09b7bdf970e5fd6c01ab691923b
Joseph Francis Re <josephfrancisre@gmail.com>
2023-03-06 04:22:58 -0500

    Corrections to moving averages

--- Stocks/Trading System/moving_averages_trading_system.py
+++ Stocks/Trading System/moving_averages_trading_system.py
@@ -17,11 +17,11 @@ data['21-day'] = data['Close'].rolling(21).mean()
 data['Change'] = np.log(data.Close / data.Close.shift())
 
 with plt.style.context('ggplot'):
-    plt.figure(figsize=(8,6))
+    plt.figure(0, figsize=(8,6))
     plt.plot(data.Close[:])
     plt.plot(data['9-day'][:], label='9-day moving average')
     plt.plot(data['21-day'][:], label='21-day moving average')
-    plt.legend(loc=1)
+    plt.legend(loc=2)
     plt.title('NVIDIA Stock Price Moving Averages')
     plt.ylabel('Close Price (USD)')
     plt.xlabel('Time')
@@ -31,6 +31,13 @@ with plt.style.context('ggplot'):
 data['position'] = np.where(data['9-day'] > data['21-day'], 1, 0)
 data['position'] = np.where(data['9-day'] < data['21-day'], -1, data['position'])
 
-# Calculate change
+# Calculate and plot change
 data['Trading System'] = data['position'] * data['Change']
-data[['Change', 'Trading System']].cumsum().plot()
+plt.figure(1)
+plt.plot(data['Change'].cumsum(), label='Actual')
+plt.plot(data['Trading System'].cumsum(), label='Trading System')
+plt.legend(loc=2)
+plt.title('NVIDIA Actual Change vs. Moving Average Trading System')
+plt.ylabel('Close Price (USD)')
+plt.xlabel('Percentage Change')
+plt.savefig('percent_change_actual_vs_trading_system.png', bbox_inches='tight')
