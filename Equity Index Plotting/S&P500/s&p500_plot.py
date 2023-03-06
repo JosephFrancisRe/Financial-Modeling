9d7da1b82be265b19520a026a002bab0d64de9bb
Joseph Francis Re <josephfrancisre@gmail.com>
2023-03-05 20:25:59 -0500

    Implemented S&P500 .csv reader files

---
+++ Equity Index Plotting/S&P500/s&p500_plot.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  5 15:49:42 2023
+
+@author: JosephRe
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv('sp500_1950_mon.csv')
+
+plt.title('S&P500 Adjusted Close Price Since 1950')
+plt.ylabel('Adjusted Clase Price')
+plt.xlabel('Months')
+plt.plot(data['Adj Close'])
+plt.savefig('sp500_close_price_1950_thru_2018.png', bbox_inches='tight')
